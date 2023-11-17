d25c7e55443bd8c49a3dd95daab37a4f983d6573
HeChu <2296282722@qq.com>
2023-11-17 19:49:13 +0800

    #feat: add ToDoThing.ts

---
+++ toDoList/src/data/ToDoThing.ts
@@ -0,0 +1,35 @@
+class ToDoThing {
+  constructor(
+    public title?: string,
+    public description?: string,
+    public startDateTime?: Date,
+    public endDateTime?: Date,
+    public isDone?: boolean
+  ) {
+  }
+
+  public withTitle(title: string): ToDoThing {
+    this.title = title;
+    return this;
+  }
+
+  public withDescription(description: string): ToDoThing {
+    this.description = description;
+    return this;
+  }
+
+  public StartAt(startDateTime: Date): ToDoThing {
+    this.startDateTime = startDateTime;
+    return this;
+  }
+
+  public EndAt(endDateTime: Date): ToDoThing {
+    this.endDateTime = endDateTime;
+    return this;
+  }
+
+  public Done(isDone: boolean): ToDoThing {
+    this.isDone = isDone;
+    return this;
+  }
+}
