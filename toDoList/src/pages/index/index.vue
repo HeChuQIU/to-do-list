442a8e32b6abd7883e4cc592af6a68ffac196697
HeChu <2296282722@qq.com>
2023-11-21 23:35:03 +0800

    修正 index.vue

--- toDoList/src/pages/index/index.vue
+++ toDoList/src/pages/index/index.vue
@@ -22,9 +22,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Helloaa')
-import {ref} from 'vue'
+import { ref } from 'vue';
 import {ToDoThing} from "@/data/ToDoThing";
 import ToDoThingList from "@/components/ToDoThingList.vue";
 
